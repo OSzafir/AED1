206989e2fea2c37cfdfff1a42cd56a93a7aa829a
Luan Szafir Cordovil Nonato <149093006+OSzafir@users.noreply.github.com>
2023-12-18 01:44:10 -0300

    Update polinomio.c

--- ListaLigadaDinâmica/polinomio.c
+++ ListaLigadaDinâmica/polinomio.c
@@ -3,13 +3,13 @@
 #include <stdlib.h>
 
 void insertSorted(Polynomial* poly, int coef, int exp) {
-    // Implementação da função insertSorted
+    
 }
 
 Polynomial* addPolynomials(Polynomial* poly1, Polynomial* poly2) {
-    // Implementação da função addPolynomials
+
 }
 
 void printPolynomial(Polynomial* poly) {
-    // Implementação da função printPolynomial
+
 }
